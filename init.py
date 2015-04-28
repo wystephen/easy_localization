dd246969f838887fe8de78b8d779f8bde2600296
WangYan <551619855@qq.com>
2015-04-28 22:38:33 +0800

    乱七八糟的一次更新
    bad 效果非常差

--- init.py
+++ init.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import  svm
+from sklearn.tree import   DecisionTreeClassifier
 
 
 #import sklearn.
@@ -23,16 +24,27 @@ def read_data():
     for j in range(len(file_list)):
         for i in range(len(file_list)):
             file_name = file_list[i]
-            print type(file_name)
+            #print type(file_name)
             if len(file_name) < 5:
                 file_list.pop(i)
                 break
 
     return file_list
 
+def read_test_data():
+    file_list = os.listdir('test_RSS')
+    for j in range(len(file_list)):
+        for i in range(len(file_list)):
+            file_name = file_list[i]
+            #print type(file_name)
+            if len(file_name) < 5:
+                file_list.pop(i)
+                break
+
+    return file_list
 
 # 输入wifi文件，和mac——list文件，并且处理
-def file_trance(file, file_index):
+def file_trance(file, file_index, file_dir = 'RSS/'):
     fp = open(file, 'rb')
     wifi = fp.readlines()
     fp.close()
@@ -40,12 +52,12 @@ def file_trance(file, file_index):
     if (os.path.exists(r'mac_list.txt')) == True:
         fp = open('mac_list.txt', 'rb')
         mac_list = fp.readlines()
-        print 'mac_list:', mac_list
+        #print 'mac_list:', mac_list
         fp.close()
-    fout = open(str('RSS/'+ str(file_index)), 'w')
+    fout = open(str(file_dir+ str(file_index)), 'w')
     all_instance = np.zeros(len(mac_list) + 1)
     first = True
-    print mac_list
+    #print mac_list
     for line in wifi:
         if line[0] == '@':
             continue
@@ -74,12 +86,52 @@ def file_trance(file, file_index):
                 num = mac_list.index(str(line[0]))
                 all_instance[num] = line[1]
 
+def file_trance_temp(file, file_index, file_dir = 'RSS/'):
+    fp = open(file, 'rb')
+    wifi = fp.readlines()
+    fp.close()
+
+    if (os.path.exists(r'mac_list.txt')) == True:
+        fp = open('mac_list.txt', 'rb')
+        mac_list = fp.readlines()
+        #print 'mac_list:', mac_list
+        fp.close()
+    fout = open(str(file_dir+ str(file_index)), 'w')
+    all_instance = np.zeros(len(mac_list) + 1)
+    first = True
+    #print mac_list
+    for line in wifi:
+        if line[0] == '@':
+            continue
+        if line[0] == '#':
+            if not first:
+                for i in range(len(mac_list) + 1):
+                    fout.write((str(int(all_instance[i])) + ' '))
+            fout.write('\n')
+            line = line.split(';')
+            #thetime = time.mktime([int(year), int(month), int(day), int(hours), int(line[0]), int(line[1]), 0, 0, 0])
+            # print thetime
+            #thetime = thetime + float(line[2]) / 1000
+            # print 'thetime: ' , thetime
+            #fout.write(str(thetime))
+            #fout.write(' ')
+            first = False
+        else:
+            # print 'line:', line
+            line = line.split(';')
+            if str(line[0]) + '\r\n' in mac_list:
+                num = mac_list.index(str(line[0]) + '\r\n')
+                all_instance[num] = line[2]
+            if str(line[0]) in mac_list:
+                num = mac_list.index(str(line[0]))
+                all_instance[num] = line[2]
+
 def data_pre():
     point_num = len(os.listdir('RSS/')) /2.0
     all_data_in = list()
     all_data_out = list()
 
-    for i in range(0,int(point_num-1)):
+    for i in range(0, int(point_num-1)):
         tmp_data_in = np.loadtxt(str('RSS/'+ str(i)))
         for j in range(len(tmp_data_in[:,1])):
             all_data_in.append(tmp_data_in[j,:])
@@ -120,10 +172,11 @@ if __name__ == '__main__':
         file_trance(str('RSS/'+ the_file_list[i]),i)
     data_in, data_out = data_pre()
     print len(data_in), len(data_out)
-    train_data_in, train_data_out, valid_data_in, valid_data_out = data_witeout(data_in,data_out,0.7)
+    train_data_in, train_data_out, valid_data_in, valid_data_out = data_witeout(data_in,data_out,0.9)
 
-    clf = KNeighborsClassifier(n_neighbors=5)
-    #clf = svm.SVC()
+    #clf = DecisionTreeClassifier()
+    clf = KNeighborsClassifier(n_neighbors=10)
+    #clf = svm.SVC(kernel='linear')
     clf.fit(train_data_in,train_data_out)
 
     pre_out = clf.predict(valid_data_in)
@@ -138,4 +191,26 @@ if __name__ == '__main__':
     print 'acc:', err_times /1.0/len(pre_out)
     plt.plot(pre_out-valid_data_out)
     plt.grid()
+
+    test_file_list = read_test_data()
+    for i in range(len(test_file_list)):
+        file_trance('test_RSS/'+test_file_list[i] , i ,file_dir='test_RSS/')
+    plt.figure('new_test')
+
+    for i in range(len(test_file_list)):
+        temp_in = np.loadtxt(str('test_RSS/')+str(i))
+
+        plt.plot(clf.predict(temp_in))
+
+    file_trance_temp('WLAN_FP_RSSI_NGI.log',1,file_dir='')
+    test_other  = np.loadtxt('1',dtype='int')
+
+
+    for i in range(len(test_other[:,1])):
+        for j in range(len(test_other[1,:])):
+            test_other[i,j] = int(100 - test_other[i,j])
+    print test_other.dtype
+    np.savetxt('2',test_other)
+    plt.figure('other')
+    plt.plot(clf.predict(test_other))
     plt.show()
